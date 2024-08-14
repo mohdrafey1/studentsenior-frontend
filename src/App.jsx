78523ad73f1e4ed17c24270749bb443555c9a83b
mohdrafey1 <mohdrafey600@gmail.com>
2024-08-14 11:34:56 +0530

    fontend repo added

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h2 className="text-5xl">hello everyone</h2>
+        </div>
+    );
+}
+
+export default App;
