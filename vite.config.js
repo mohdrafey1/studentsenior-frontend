a2c164e41fb4ac7ef15bc281059d8810b405adf9
mohdrafey1 <mohdrafey600@gmail.com>
2024-08-22 20:11:10 +0530

    updated sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { createSitemapPlugin } from 'vite-plugin-sitemap';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+        react(),
+        createSitemapPlugin({
+            hostname: 'https://studentsenior.com',
+            outDir: 'dist',
+            dynamicRoutes: [
+                '/college/:collegeName',
+                '/college/:collegeName/seniors',
+                '/college/:collegeName/pyq',
+                '/college/:collegeName/store',
+                '/college/:collegeName/community',
+                '/college/:collegeName/whatsapp-group',
+                '/college/:collegeName/opportunities',
+                '/college/:collegeName/notes',
+            ],
+        }),
+    ],
+});
