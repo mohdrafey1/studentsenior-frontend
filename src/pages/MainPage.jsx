0c92e8350353e37648c2905b056d1daa92152ed9
Sahil-Coder1 <26maysahil@gmail.com>
2024-09-01 15:29:04 +0530

    Add Whatsapp group feature

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -25,6 +25,10 @@ const MainPage = () => {
         {
             "id": "66d08aff784c9f07a53507b9",
             "name": "GCET Noida",
+        },
+        {
+            "id": "66d40833ec7d66559acbf24c",
+            "name": "KMC UNIVERSITY",
         }
     ];
     useEffect(() => {
