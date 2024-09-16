5f8789888a719c23c1c3eec470f0aaec75c121c8
mohdrafey1 <mohdrafey600@gmail.com>
2024-09-17 00:56:55 +0530

    bug

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
                 theme_color: '#000000',
                 icons: [
                     {
-                        src: '/logo.jpg',
+                        src: '/logo.png',
                         sizes: '192x192',
                         type: 'image/png',
                     },
