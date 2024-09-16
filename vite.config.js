1286aa6231bb937af93047e8baa5950832b2b157
mohdrafey1 <mohdrafey600@gmail.com>
2024-09-17 01:21:38 +0530

    logo update

--- vite.config.js
+++ vite.config.js
@@ -18,12 +18,12 @@ export default defineConfig({
                 theme_color: '#000000',
                 icons: [
                     {
-                        src: '/logo.png',
+                        src: '/image.png',
                         sizes: '192x192',
                         type: 'image/png',
                     },
                     {
-                        src: '/logo.png',
+                        src: '/image.png',
                         sizes: '512x512',
                         type: 'image/png',
                     },
