fc5f474e651f3ff9b3bebbce99dcabc98db11c98
Sahil-Coder1 <26maysahil@gmail.com>
2024-10-12 18:29:50 +0530

    About College

--- src/components/About/CollegeAbout.jsx
+++ src/components/About/CollegeAbout.jsx
@@ -1,21 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { API_BASE_URL, API_KEY } from '../../config/apiConfiguration';
 
 const CollegeAbout = () => {
+
+    const [collegeData, setCollegeData] = useState([]);
+    const [des, setDes] = useState('');
+
+    const fetchCollege = async () => {
+        try {
+          const response = await fetch(`${API_BASE_URL}/api/colleges`, {
+            headers: {
+              'Content-Type': 'application/json',
+              'x-api-key': API_KEY,
+            },
+            method: 'GET',
+          });
+          const Collegedata = await response.json();
+          setCollegeData(Collegedata);
+          setDes(collegeBased(Collegedata));
+        } catch (error) {
+          console.error('Error fetching college data:', error);
+        }
+      };
+
+      const collegeBased = (data) =>{
+        let description = '';
+        for(let i = 0; i<data.length; i++){
+            const collegeId = localStorage.getItem('id');
+            if(data[i]._id === collegeId){
+                description = (data[i].description);
+            }
+        }
+        return description;
+      }
+
+    useEffect(() => {
+        fetchCollege();
+    },[])
     return (
         <section className="flex justify-center ">
             <div className="text-center mx-4/5 max-w-7xl p-8">
                 <h3 className="text-3xl font-bold mb-6">About Us</h3>
                 <p className="text-gray-500 text-xl">
-                    Integral University in Lucknow, established in 2004, is a
-                    prominent private institution recognized for its diverse
-                    range of academic programs and strong emphasis on research
-                    and innovation. Spread over a sprawling 120-acre campus, the
-                    university offers modern facilities, including
-                    state-of-the-art labs, libraries, hostels, and sports
-                    complexes. It provides over 100 undergraduate, postgraduate,
-                    and doctoral courses across various disciplines such as
-                    engineering, management, science, law, education, and
-                    pharmacy...
+                    {des}
                 </p>
             </div>
         </section>
