096b38a0f22788b02ba45d0c40c4aa8b74ef80bd
mohdrafey1 <mohdrafey600@gmail.com>
2024-10-25 17:28:51 +0530

    collected the api

--- src/config/apiConfiguration.js
+++ src/config/apiConfiguration.js
@@ -1,2 +1,33 @@
 export const API_BASE_URL = import.meta.env.VITE_API_URL;
 export const API_KEY = import.meta.env.VITE_API_KEY;
+
+export const api = {
+    auth: {
+        login: `${API_BASE_URL}/api/auth/signin`,
+        signup: `${API_BASE_URL}/api/auth/signup`,
+        google: `${API_BASE_URL}/api/auth/google`,
+        signout: `${API_BASE_URL}/api/auth/signout`,
+    },
+
+    user: `${API_BASE_URL}/api/update`,
+
+    college: `${API_BASE_URL}/api/colleges`,
+
+    community: `${API_BASE_URL}/api/community/posts`,
+
+    contactus: `${API_BASE_URL}/api/contactus`,
+
+    notes: `${API_BASE_URL}/api/notes`,
+
+    getOpportunity: `${API_BASE_URL}/api/opportunity/getopportunities`,
+
+    giveOpportunity: `${API_BASE_URL}/api/opportunity/giveopportunities`,
+
+    pyq: `${API_BASE_URL}/api/pyqs`,
+
+    group: `${API_BASE_URL}/api/whatsappgroup`,
+
+    senior: `${API_BASE_URL}/api/seniors`,
+
+    store: `${API_BASE_URL}/api/store`,
+};
