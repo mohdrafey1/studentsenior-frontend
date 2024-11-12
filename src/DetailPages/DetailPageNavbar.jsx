4c88cf73cabb127ca6d58151ae0ad6c3ba8df97f
mohdrafey1 <mohdrafey600@gmail.com>
2024-11-12 13:37:10 +0530

    shorten url implement

--- src/DetailPages/DetailPageNavbar.jsx
+++ src/DetailPages/DetailPageNavbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useNavigate, useLocation, useParams } from 'react-router-dom';
+import { API_KEY_URL } from '../config/apiConfiguration';
+import { toast } from 'react-toastify';
 
 const DetailPageNavbar = ({ path }) => {
     const { collegeName } = useParams();
@@ -14,17 +16,44 @@ const DetailPageNavbar = ({ path }) => {
         }
     };
 
-    const handleShare = () => {
+    const handleShare = async () => {
         const postUrl = window.location.href;
-        if (navigator.share) {
-            navigator
-                .share({ title: 'Student Senior ', url: postUrl })
-                .catch((error) => console.log('Share failed:', error));
-        } else {
-            navigator.clipboard
-                .writeText(postUrl)
-                .then(() => alert('Link copied to clipboard!'))
-                .catch(() => alert('Failed to copy link.'));
+
+        try {
+            const response = await fetch(
+                'https://ssurl.studentsenior.com/api/shorten',
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'x-api-key': API_KEY_URL,
+                    },
+                    body: JSON.stringify({ originalUrl: postUrl }),
+                }
+            );
+
+            if (response.ok) {
+                const data = await response.json();
+                const shortUrl = data.shortUrl;
+
+                if (navigator.share) {
+                    navigator
+                        .share({ title: 'Student Senior', url: shortUrl })
+                        .catch((error) => console.log('Share failed:', error));
+                } else {
+                    navigator.clipboard
+                        .writeText(shortUrl)
+                        .then(() =>
+                            toast.success('Shortened link copied to clipboard!')
+                        )
+                        .catch(() => alert('Failed to copy link.'));
+                }
+            } else {
+                toast.error('Failed to shorten the URL.');
+            }
+        } catch (error) {
+            console.log('Error shortening the URL:', error);
+            toast.error('Failed to shorten the URL.');
         }
     };
 
--- src/config/apiConfiguration.js
+++ src/config/apiConfiguration.js
@@ -1,5 +1,6 @@
 export const API_BASE_URL = import.meta.env.VITE_API_URL;
 export const API_KEY = import.meta.env.VITE_API_KEY;
+export const API_KEY_URL = import.meta.env.VITE_API_KEY_URL;
 
 export const api = {
     auth: {
