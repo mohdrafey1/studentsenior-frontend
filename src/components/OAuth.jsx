ffd2b84d462a52197e1e46c86e76f091279598b5
Sahil-Coder1 <26maysahil@gmail.com>
2024-11-15 15:02:21 +0530

    login page updated

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,91 +1,93 @@
-import { useState } from 'react';
-import { GoogleAuthProvider, signInWithPopup, getAuth } from 'firebase/auth';
-import { app } from '../firebase';
-import { useDispatch } from 'react-redux';
-import { signInSuccess } from '../redux/user/userSlice';
-import { useNavigate, useLocation } from 'react-router-dom';
-import { api, API_KEY } from '../config/apiConfiguration';
-import { toast } from 'react-toastify';
+import { useState } from "react";
+import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
+import { app } from "../firebase";
+import { useDispatch } from "react-redux";
+import { signInSuccess } from "../redux/user/userSlice";
+import { useNavigate, useLocation } from "react-router-dom";
+import { api, API_KEY } from "../config/apiConfiguration";
+import { toast } from "react-toastify";
 
 export default function OAuth() {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const location = useLocation();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-    const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-    const handleGoogleClick = async () => {
-        setLoading(true);
+  const handleGoogleClick = async () => {
+    setLoading(true);
 
-        try {
-            const provider = new GoogleAuthProvider();
-            const auth = getAuth(app);
+    try {
+      const provider = new GoogleAuthProvider();
+      const auth = getAuth(app);
 
-            const result = await signInWithPopup(auth, provider);
-            const res = await fetch(`${api.auth.google}`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'x-api-key': API_KEY,
-                },
-                credentials: 'include',
-                body: JSON.stringify({
-                    name: result.user.displayName,
-                    email: result.user.email,
-                    photo: result.user.photoURL,
-                }),
-            });
+      const result = await signInWithPopup(auth, provider);
+      const res = await fetch(`${api.auth.google}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "x-api-key": API_KEY,
+        },
+        credentials: "include",
+        body: JSON.stringify({
+          name: result.user.displayName,
+          email: result.user.email,
+          photo: result.user.photoURL,
+        }),
+      });
 
-            const data = await res.json();
-            // console.log(data);
-            dispatch(signInSuccess(data));
-            const from =
-                location.state?.from?.pathname + location.state?.from?.search ||
-                '/';
-            navigate(from, { replace: true });
-            toast.success('Log in successfull');
-        } catch (error) {
-            console.log('Could not login with Google', error);
-            toast.error('login failed');
-        } finally {
-            setLoading(false);
-        }
-    };
+      const data = await res.json();
+      // console.log(data);
+      dispatch(signInSuccess(data));
+      const from =
+        location.state?.from?.pathname + location.state?.from?.search || "/";
+      navigate(from, { replace: true });
+      toast.success("Log in successfull");
+    } catch (error) {
+      console.log("Could not login with Google", error);
+      toast.error("login failed");
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    return (
-        <button
-            type="button"
-            onClick={handleGoogleClick}
-            className="bg-red-700 text-white rounded-lg p-3 uppercase hover:opacity-95"
-            disabled={loading}
-        >
-            {loading ? (
-                <div className="flex items-center justify-center">
-                    <svg
-                        className="animate-spin h-5 w-5 mr-3 text-white"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                    >
-                        <circle
-                            className="opacity-25"
-                            cx="12"
-                            cy="12"
-                            r="10"
-                            stroke="currentColor"
-                            strokeWidth="4"
-                        ></circle>
-                        <path
-                            className="opacity-75"
-                            fill="currentColor"
-                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"
-                        ></path>
-                    </svg>
-                    Logging in...
-                </div>
-            ) : (
-                'Continue with Google'
-            )}
-        </button>
-    );
+  return (
+    <div className="cursor-pointer">
+      <p
+        className="text-center bg-red-200 p-2 rounded-3xl mx-4"
+        onClick={handleGoogleClick}
+        disabled={loading}
+      >
+       
+      
+        {loading ? (
+          <div className="flex items-center justify-center ">
+            <svg
+              className="animate-spin h-5 w-5 mr-3 text-white"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"
+              ></path>
+            </svg>
+            Logging in...
+          </div>
+        ) : (
+            <i class="fa-brands fa-google text-sky-500"></i>
+        )}
+     </p>
+    </div>
+  );
 }
--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,96 +1,112 @@
-import { useState } from 'react';
-import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { useState } from "react";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import {
-    signInStart,
-    signInSuccess,
-    signInFailure,
-} from '../redux/user/userSlice';
-import { useDispatch, useSelector } from 'react-redux';
-import OAuth from '../components/OAuth';
-import { api, API_KEY } from '../config/apiConfiguration.js';
-import { toast } from 'react-toastify';
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import OAuth from "../components/OAuth";
+import { api, API_KEY } from "../config/apiConfiguration.js";
+import { toast } from "react-toastify";
+import icon from "../../public/assets/image192.png";
+import login from "../../public/assets/login.png";
 
 export default function SignIn() {
-    const [formData, setFormData] = useState({});
-    const { loading, error } = useSelector((state) => state.user);
-    const navigate = useNavigate();
-    const location = useLocation();
-    const dispatch = useDispatch();
+  const [formData, setFormData] = useState({});
+  const { loading, error } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const dispatch = useDispatch();
 
-    const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.id]: e.target.value });
-    };
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            dispatch(signInStart());
-            const res = await fetch(`${api.auth.login}`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'x-api-key': API_KEY,
-                },
-                credentials: 'include',
-                body: JSON.stringify(formData),
-            });
-            const data = await res.json();
-            if (data.success === false) {
-                dispatch(signInFailure(data));
-                return;
-            }
-            dispatch(signInSuccess(data));
-            toast.success('Sign In successful');
-            const from =
-                location.state?.from?.pathname + location.state?.from?.search ||
-                '/';
-            navigate(from, { replace: true });
-        } catch (error) {
-            dispatch(signInFailure(error));
-            toast.error('Sign In Failed');
-        }
-    };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(signInStart());
+      const res = await fetch(`${api.auth.login}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "x-api-key": API_KEY,
+        },
+        credentials: "include",
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(signInFailure(data));
+        return;
+      }
+      dispatch(signInSuccess(data));
+      toast.success("Sign In successful");
+      const from =
+        location.state?.from?.pathname + location.state?.from?.search || "/";
+      navigate(from, { replace: true });
+    } catch (error) {
+      dispatch(signInFailure(error));
+      toast.error("Sign In Failed");
+    }
+  };
 
-    return (
-        <>
-            {/* <Header /> */}
-            <div className="p-3 max-w-lg mx-auto my-24">
-                <h1 className="text-3xl text-center font-semibold my-7">
-                    Sign In
-                </h1>
-                <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-                    <input
-                        type="email"
-                        placeholder="Email"
-                        id="email"
-                        className="bg-slate-100 p-3 rounded-lg"
-                        onChange={handleChange}
-                    />
-                    <input
-                        type="password"
-                        placeholder="Password"
-                        id="password"
-                        className="bg-slate-100 p-3 rounded-lg"
-                        onChange={handleChange}
-                    />
-                    <button
-                        disabled={loading}
-                        className="bg-sky-500 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
-                    >
-                        {loading ? 'Loading...' : 'Sign In'}
-                    </button>
-                    <OAuth />
-                </form>
-                <div className="flex gap-2 mt-5">
-                    <p>Don't Have an account?</p>
-                    <Link to="/sign-up">
-                        <span className="text-blue-500">Sign up</span>
-                    </Link>
-                </div>
-                <p className="text-red-700 mt-5">
-                    {error ? error.message || 'Something went wrong!' : ''}
-                </p>
+  return (
+    <>
+      {/* <Header /> */}
+      <div className="lg:flex bg-gradient-to-t from-sky-200 to bg-white">
+        <div className="p-3 max-w-lg lg:mx-auto lg:my-5 lg:bg-white lg:w-2/4 py-10 rounded-xl mx-5 my-10">
+          <img src={icon} alt="logo" className="h-20 w-20 mx-auto" />
+          <h1 className="text-2xl text-center font-semibold mt-7">
+            Welcome Back
+          </h1>
+          <p className="text-center mb-7 text-xs text-slate-800">
+            Please enter your details to sign in
+          </p>
+          <form onSubmit={handleSubmit} className="flex flex-col gap-0">
+            <label className="mx-3 my-0">Email</label>
+            <input
+              type="email"
+              placeholder="Enter your email"
+              id="email"
+              className="bg-slate-100 p-2 ring-1 px-4 rounded-lg mx-3 mb-3"
+              onChange={handleChange}
+            />
+            <label className="mx-3 ">Password</label>
+            <input
+              type="password"
+              placeholder="Password"
+              id="password"
+              className="bg-slate-100 p-2 ring-1 px-4 rounded-lg mx-3"
+              onChange={handleChange}
+            />
+            <br />
+            <button
+              disabled={loading}
+              className="bg-sky-500 text-white p-2 rounded-lg uppercase hover:opacity-95 disabled:opacity-80 mx-3"
+            >
+              {loading ? "Signing in ..." : "Sign In"}
+            </button>
+            <br />
+            <div className="flex">
+              <div className=" h-0.5 bg-slate-400 w-full my-3 ml-4 mr-2 rounded-3xl"></div>
+              or
+              <div className="h-0.5 bg-slate-400 w-full my-3 ml-2 mr-4 rounded-3xl"></div>
             </div>
-        </>
-    );
+            <OAuth />
+          </form>
+          <div className="flex gap-2 mt-5 justify-center">
+            <p>Don't Have an account?</p>
+            <Link to="/sign-up">
+              <span className="text-blue-500">Sign up</span>
+            </Link>
+          </div>
+        </div>
+        <div className=" w-2/5 flex content-center">
+          <img src={login} alt="illustrature" className="h-2/3 mx-auto my-auto lg:block hidden" />
+        </div>
+      </div>
+    </>
+  );
 }
