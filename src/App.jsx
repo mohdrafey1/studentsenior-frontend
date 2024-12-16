5867e452fc5462988482899053ace8885e79c3b2
mohdrafey1 <mohdrafey600@gmail.com>
2024-12-16 13:04:20 +0530

    auto refresh on updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import ScrollToTop from './utils/ScrollToTop';
 import RoutesComponent from './routesComponent';
 
 const App = () => {
+    useEffect(() => {
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker.ready.then((registration) => {
+                registration.addEventListener('statechange', (event) => {
+                    if (event.target.state === 'activated') {
+                        window.location.reload();
+                    }
+                });
+            });
+        }
+    }, []);
+
     return (
         <Router>
             <ScrollToTop />
