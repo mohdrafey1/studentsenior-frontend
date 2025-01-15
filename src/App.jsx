6d16da3a919b9ca9a1ea6cb65618a17ab3246a10
mohdrafey1 <mohdrafey600@gmail.com>
2025-01-16 02:31:24 +0530

    no authcheck for now

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,11 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import ScrollToTop from './utils/ScrollToTop';
 import RoutesComponent from './routesComponent';
-import AuthCheck from './hooks/authCheck';
 
 const App = () => {
     return (
         <Router>
             <ScrollToTop />
-            <AuthCheck />
             <Layout>
                 <RoutesComponent />
             </Layout>
